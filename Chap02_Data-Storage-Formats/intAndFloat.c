67303a2a348d1b661387007af117a3568a181fc8
winesitter <florian.setzwein@gmx.de>
2018-08-05 12:56:55 +0200

    Chap02_Data-Storage-Formats/

---
+++ Chap02_Data-Storage-Formats/intAndFloat.c
@@ -0,0 +1,19 @@
+/*
+ *  intAndFloat.c
+ *  Using printf to display integer and a float.
+ *
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  int anInt = 19088743;
+  float aFloat = 19088.743;
+  
+  printf("The integer is %i and the float is %f\n", anInt, aFloat);
+
+  return 0;
+
+
+}
